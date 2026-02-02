2d0fdf3791dfc5ed5bcbebaaa14a93501e4b9304
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-02 19:57:12 -0400

    Agrega el modelo ViajesEspaciales

---
+++ Models/ViajesEspaciales.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Raydelis_HilarioAP1_P1.Models;
+
+public class ViajesEspaciales
+{
+    [Key]
+    public int ViajeId { get; set; }
+    [Required(ErrorMessage = "El campo fecha de viaje es obligatorio.")]
+    public DateTime FechaViaje { get; set; }
+    [Required(ErrorMessage = "El campo descripcion es obligatorio.")]
+    public string Descripcion { get; set; }
+    [Required(ErrorMessage = "El campo costo es obligatorio.")]
+    public double Costo { get; set; }
+}
