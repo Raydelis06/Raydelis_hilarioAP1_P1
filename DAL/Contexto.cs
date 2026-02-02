5fa91b15e3f807ef339e02a96a978812c874530e
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-02 18:48:14 -0400

    Agrega carpetas y contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Raydelis_HilarioAP1_P1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
