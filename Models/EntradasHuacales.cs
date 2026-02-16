2c5bc69b9a59526e9975d46dbd0a56e0e98d2939
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-16 17:51:06 -0400

    Agrega validaciones para numeros

--- Models/EntradasHuacales.cs
+++ Models/EntradasHuacales.cs
@@ -11,7 +11,9 @@ public class EntradasHuacales
     [Required(ErrorMessage ="El 'Nombre del cliente' es obligatorio.")]
     public string NombreCliente { get; set; }
     [Required(ErrorMessage ="El campo 'Cantidad' es obligatorio.")]
+    [Range(1, double.MaxValue, ErrorMessage = "La cantidad debe ser mayor que 0")]
     public int Cantidad { get; set; }
     [Required(ErrorMessage ="El campo 'Precio' es obligatorio.")]
+    [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
     public double Precio { get; set; }
 }
