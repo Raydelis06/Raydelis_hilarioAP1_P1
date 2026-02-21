e1ca13260ae93337b2b1195b09247803a6cac632
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-21 11:07:57 -0400

    Agrega valor de fecha por defecto

--- Models/EntradasHuacales.cs
+++ Models/EntradasHuacales.cs
@@ -7,7 +7,7 @@ public class EntradasHuacales
     [Key]
     public int IdEntrada { get; set; }
     [Required(ErrorMessage = "El campo 'Fecha' es obligatorio.")]
-    public DateTime Fecha { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
     [Required(ErrorMessage = "El 'Nombre del cliente' es obligatorio.")]
     public string NombreCliente { get; set; } = string.Empty;
     [Required(ErrorMessage ="El campo 'Cantidad' es obligatorio.")]
