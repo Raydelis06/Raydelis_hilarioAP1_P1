260811a1eac2198185e7132bc37152023824469d
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-28 21:49:27 -0400

    Agrega la clase DetalleEntrada

---
+++ Models/DetalleEntrada.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Raydelis_HilarioAP1_P1.Models;
+
+public class DetalleEntrada
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int EntradaId { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public ICollection<TiposHuacales> TiposHuacales { get; set; }
+
+}
