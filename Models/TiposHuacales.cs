df8e9042881d3d8c84c807e613c338b0b0ef7442
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-28 21:32:03 -0400

    Agrega clase TiposHuacales

---
+++ Models/TiposHuacales.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Raydelis_HilarioAP1_P1.Models
+{
+    public class TiposHuacales
+    {
+        [Key]
+        public int TipoId { get; set;  }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Descripcion { get; set; } = null!;
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El valor debe ser mayor que 0")]
+        public int Existencia { get; set; }
+    }
+}
