4d3ae1d9d3f478b808aac9ff469894a94a1674cf
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-28 21:54:34 -0400

    Modifica llave foranea IdEntrada

---
+++ Models/DetallesEntrada.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Raydelis_HilarioAP1_P1.Models;
+
+public class DetallesEntrada
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int IdEntrada { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public int TipoId { get; set; }
+
+}
