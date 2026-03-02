8a4526c04db48da0961bebabbd76ae16360b51f7
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-03-01 20:11:46 -0400

    Cambia cantidad y precio de entradasHuacales a detalleEntrada

--- Models/DetallesEntrada.cs
+++ Models/DetallesEntrada.cs
@@ -9,5 +9,10 @@ public class DetallesEntrada
     public int IdEntrada { get; set; }
     [Required(ErrorMessage = "Este campo es obligatorio")]
     public int TipoId { get; set; }
-
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [Range(1, double.MaxValue, ErrorMessage = "La cantidad debe ser mayor que 0")]
+    public int Cantidad { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
+    public double Precio { get; set; }
 }
--- Models/EntradasHuacales.cs
+++ Models/EntradasHuacales.cs
@@ -11,12 +11,6 @@ public class EntradasHuacales
     public DateTime Fecha { get; set; } = DateTime.Now;
     [Required(ErrorMessage = "El 'Nombre del cliente' es obligatorio.")]
     public string NombreCliente { get; set; } = string.Empty;
-    [Required(ErrorMessage ="El campo 'Cantidad' es obligatorio.")]
-    [Range(1, double.MaxValue, ErrorMessage = "La cantidad debe ser mayor que 0")]
-    public int Cantidad { get; set; }
-    [Required(ErrorMessage ="El campo 'Precio' es obligatorio.")]
-    [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
-    public double Precio { get; set; }
 
     [ForeignKey("IdEntrada")]
     public ICollection<DetallesEntrada> DetallesEntrada { get; set; } = new List<DetallesEntrada>();
